c0f1eb53ef7a8d76ca625e51ef0bd1fbe73c612a
Robin Smorenburg <robin@smorenburg.io>
2022-01-18 19:04:09 +0100

    Add resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,9 @@ provider "google-beta" {
 locals {
   apis = [
     "compute.googleapis.com",
-    "container.googleapis.com"
+    "container.googleapis.com",
+    "secretmanager.googleapis.com",
+    "cloudkms.googleapis.com"
   ]
   cluster_01_service_account_roles = [
     "roles/logging.logWriter",
@@ -37,6 +39,7 @@ locals {
     "roles/stackdriver.resourceMetadata.writer",
     "roles/compute.securityAdmin"
   ]
+  cluster_01_observer_service_account_roles = []
 }
 
 # Enable the APIs
@@ -44,6 +47,63 @@ resource "google_project_service" "apis" {
   for_each           = toset(local.apis)
   service            = each.value
   disable_on_destroy = false
+}
+
+# Create the service accounts and IAM bindings.
+resource "google_service_account" "cluster_01" {
+  account_id = "cluster-01"
+
+  depends_on = [google_project_service.apis]
+}
+
+resource "google_service_account" "cluster_01_observer" {
+  account_id = "cluster-01-observer"
+
+  depends_on = [google_project_service.apis]
+}
+
+resource "google_project_iam_member" "cluster_01_service_account_roles" {
+  for_each = toset(local.cluster_01_service_account_roles)
+  project  = var.project_id
+  role     = each.value
+  member   = "serviceAccount:${google_service_account.cluster_01.email}"
+}
+
+resource "google_project_iam_member" "cluster_01_observer_service_account_roles" {
+  for_each = toset(local.cluster_01_observer_service_account_roles)
+  project  = var.project_id
+  role     = each.value
+  member   = "serviceAccount:${google_service_account.cluster_01_observer.email}"
+}
+
+resource "google_service_account_iam_binding" "cluster_01_observer_workload_identity_user" {
+  service_account_id = google_service_account.cluster_01_observer.name
+  role               = "roles/iam.workloadIdentityUser"
+  members            = ["serviceAccount:${var.project_id}.svc.id.goog[observer/observer]"]
+}
+
+# Create the keyring, encryption keys, and IAM bindings.
+resource "google_kms_key_ring" "cluster_01" {
+  name     = "cluster-01"
+  location = "europe-west4"
+
+  depends_on = [google_project_service.apis]
+}
+
+resource "google_kms_crypto_key" "secrets" {
+  name     = "secrets"
+  key_ring = google_kms_key_ring.cluster_01.id
+}
+
+resource "google_kms_crypto_key" "observer" {
+  name     = "observer"
+  key_ring = google_kms_key_ring.cluster_01.id
+}
+
+resource "google_kms_crypto_key_iam_member" "secrets" {
+  crypto_key_id = google_kms_crypto_key.secrets.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:service-743723366099@container-engine-robot.iam.gserviceaccount.com"
 }
 
 # Create network and subnetwork, including secondary ranges and firewall rules.
@@ -148,20 +208,6 @@ resource "google_compute_global_address" "cluster_01_observer_ingress" {
   depends_on = [google_project_service.apis]
 }
 
-# Create the service account for the GKE cluster node pools and bind roles.
-resource "google_service_account" "cluster_01" {
-  account_id = "cluster-01"
-
-  depends_on = [google_project_service.apis]
-}
-
-resource "google_project_iam_member" "cluster_01_service_account_roles" {
-  for_each = toset(local.cluster_01_service_account_roles)
-  project  = var.project_id
-  role     = each.value
-  member   = "serviceAccount:${google_service_account.cluster_01.email}"
-}
-
 # Create the GKE cluster and node pool.
 resource "google_container_cluster" "cluster_01" {
   provider = google-beta
@@ -190,6 +236,17 @@ resource "google_container_cluster" "cluster_01" {
     cluster_secondary_range_name  = "pods"
     services_secondary_range_name = "services"
   }
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+
+  database_encryption {
+    state    = "ENCRYPTED"
+    key_name = google_kms_crypto_key.secrets.id
+  }
+
+  depends_on = [google_kms_crypto_key_iam_member.secrets]
 }
 
 resource "google_container_node_pool" "node_pool_01" {
@@ -210,5 +267,9 @@ resource "google_container_node_pool" "node_pool_01" {
     tags            = ["nodes"]
     service_account = google_service_account.cluster_01.email
     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
   }
 }
